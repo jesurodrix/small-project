07f8df6678cd28801074a8a4d76cf38320126419
Jesu Rodriguez <jesusayala201156@gmail.com>
2024-10-27 22:49:14 -0400

    Modificando chupetin

--- pagina_2/torta_files/5yc02qc_ueC.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("MWDidQueryPromptsNux",[],(function(a,b,c,d,e,f){"use strict";var g=!1;function a(){g=!0}function b(){return g}f.markDidQueryPromptsNux=a;f.getDidQueryPromptsNux=b}),66);
-__d("isFeatureLimitEnabled",["qex"],(function(a,b,c,d,e,f,g){"use strict";function a(){return c("qex")._("588")===!0}g["default"]=a}),98);
--- pagina_2/torta_files/Mlp9IGhL9-_.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("Dots3VerticalOutline20.svg.react",["react"],(function(a,b,c,d,e,f,g){"use strict";var h,i=h||d("react");function a(a){return i.jsxs("svg",babelHelpers["extends"]({viewBox:"0 0 20 20",width:"1em",height:"1em",fill:"currentColor"},a,{children:[a.title!=null&&i.jsx("title",{children:a.title}),a.children!=null&&i.jsx("defs",{children:a.children}),i.jsx("g",{fillRule:"evenodd",transform:"translate(-446 -398)",children:i.jsx("path",{d:"M456 410a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0-12a2 2 0 1 1 0-4 2 2 0 0 1 0 4"})})]}))}a.displayName=a.name+" [from "+f.id+"]";a._isSVG=!0;b=a;g["default"]=b}),98);
-__d("useAttachmentsMAWNoop",[],(function(a,b,c,d,e,f){"use strict";function a(a,b){return[]}f["default"]=a}),66);
